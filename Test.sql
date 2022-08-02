0b01955a717aa1299d368f80c0e1acef54cbf835
A200134737 <kadukerika@gmail.com>
2022-08-02 09:52:08 +0200

    zaciatok

---
+++ Test.sql
@@ -0,0 +1,36 @@
+-- 1 Názvy niektorých obcí v tabuľke obec sa opakujú, pretože na Slovensku existujú obce, ktoré majú rovnaký názov. Zistite:
+-- koľko je takých obcí (1 dopyt)
+-- ktorý názov obce je použitý najviac (1 dopyt)
+
+
+-- 2 Koľko okresov sa nachádza v košickom kraji?
+
+
+-- 3 A koľko má košický kraj obcí? Pri tvorbe dopytu vám môže pomôcť informácia, že trenčiansky kraj má spolu 276 obcí.
+
+
+-- 4 Zistite, ktorá obec (mesto) bola na Slovensku najväčšia v roku 2012. Pri tvorbe dopytu vám môže pomôcť informácia, že táto obec (mesto) bola najväčšia na Slovensku v rokoch 2009-2012, avšak má v populácii klesajúcu tendenciu. Vo výsledku vypíšte jej názov a počet obyvateľov.
+
+
+-- 5 Koľko obyvateľov mal okres Sabinov v roku 2012? Pri tvorbe dopytu vám môže pomôcť informácia, že okres Dolný Kubín mal v roku 2010 39553 obyvateľov.
+
+
+-- 6 Ako sme na tom na Slovensku? Vymierame alebo rastieme? Zobrazte trend vývoja populácie za jednotlivé roky a výsledok zobrazte od najnovších informácií po najstaršie.
+
+
+-- 7 Zistite, ktorá obec alebo obce boli najmenšie v okrese Tvrdošín v roku 2011. Pri tvorbe dopytu vám môže pomôcť informácia, že v okrese Ružomberok to bola v roku 2012 obec Potok s počtom obyvateľov 107.
+
+
+-- 8 Zistite všetky obce (ich názvy), ktoré mali v roku 2010 počet obyvateľov do 5000. Pri tvorbe dopytu vám môže pomôcť informácia, že v roku 2009 bolo týchto obcí o 1 viac ako v roku 2010.
+
+
+-- 9 Zistite 10 obcí s populáciou nad 20000, ktoré mali v roku 2012 najväčší pomer žien voči mužom (viac žien v obci ako mužov). Týchto 10 obcí vypíšte v poradí od najväčšieho pomeru po najmenší. Vo výsledku okrem názvu obce vypíšte aj pomer zaokrúhlený na 4 desatinné miesta. Pri tvorbe dopytu vám môže pomôcť informácia, že v roku 2011 bol tento pomer pre obec Košice  - Juh 1,1673.
+
+
+-- 10 Vypíšte sumárne informácie o stave Slovenska v roku 2012 v podobe tabuľky, ktorá bude obsahovať pre každý kraj informácie o počte obyvateľov, o počte obcí a počte okresov.
+
+
+-- 11 To, či vymierame alebo rastieme, sme už zisťovali. Ale ktoré obce sú na tom naozaj zle? Kde by sa nad touto otázkou mali naozaj zamyslieť? Zobrazte obce, ktoré majú klesajúci trend (rozdiel v populácii dvoch posledných rokov je menší ako 0) - vypíšte ich názov, počet obyvateľov v poslednom roku, počet obyvateľov v predchádzajúcom roku a rozdiel v populácii posledného oproti predchádzajúcemu roku. Zoznam utrieďte vzostupne podľa tohto rozdielu od obcí s najmenším prírastkom obyvateľov po najväčší.
+
+
+-- 12 Zistite počet obcí, ktorých počet obyvateľov v roku 2012 je nižší, ako bol slovenský priemer v danom roku.
